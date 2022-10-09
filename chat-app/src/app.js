21b60807bd0b201bf9df8fe3954d9de78a5388f6
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-10-09 20:01:32 -0300

    socket io server side programming

--- chat-app/src/app.js
+++ chat-app/src/app.js
@@ -2,15 +2,21 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const bp = require('body-parser')
+const sockeio = require('socket.io')
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 const app = express()
 const server = http.createServer(app)
+const io = sockeio(server)
 
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
 
 app.use(express.static(publicDirectoryPath))
+
+io.on('connection', () => {
+    console.log('New websocket connection')
+})
 
 module.exports = server
