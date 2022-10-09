8a4ff52a7e5ec983d59d2968be7ee855c4b041a7
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-10-09 19:57:45 -0300

    server programming refactored

--- chat-app/src/app.js
+++ chat-app/src/app.js
@@ -1,13 +1,16 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
 const bp = require('body-parser')
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 const app = express()
+const server = http.createServer(app)
+
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
 
 app.use(express.static(publicDirectoryPath))
 
-module.exports = app
+module.exports = server
