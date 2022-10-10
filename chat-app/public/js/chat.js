8e8168e5ed88520f6f8693a605ee136cdedfb735
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-10-09 23:14:52 -0300

    form refactoring and better routines

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,29 +1,41 @@
 const socket = io()
 
+const $messageForm = document.querySelector('#Message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+
 socket.on('message', (message) => {
     console.log(message)
 })
 
-document.querySelector('#Message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
     const message = e.target.elements.message.value
-    e.target.elements.message.value = ''
     e.target.elements.last.value = message
     
     let callback = () => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus();
         console.log('Message delivered')
     }
     
     socket.emit('sendMessage', message, callback)
 })
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$sendLocationButton .addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert()
     }
+    
+    $sendLocationButton.setAttribute('disabled', 'disabled')
 
     let callback = () => {
+        $sendLocationButton.removeAttribute('disabled')
         console.log('Location Shared!')
     }
 
